c2d0ae693718ba630fb3f970ca152ad2752037f8
JDH <crysisov@gmail.com>
2025-07-04 09:18:40 +0200

    hide src

--- src/js/modules/used.js
+++
@@ -1,9 +0,0 @@
-export function fillLines(percent, line) {
-
-    const counters = document.querySelectorAll(percent),
-        lines = document.querySelectorAll(line);
-
-    counters.forEach( (item, i)=> {
-        lines[i].style.width = item.innerHTML;
-    });
-}
